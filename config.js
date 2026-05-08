29f717431b8f81b39e6f33149adfa9fa3b1b12fe
tommy tips <63163780+tomtomlsg72@users.noreply.github.com>
2026-05-08 18:05:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
   position: "top_right",
   config: {
     title: "PRONOTE",
-    homeAssistantUrl: "http://192.168.1.152:8123",
-    accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YjM0NWE3NWZkZWM0Njc1OTI1MDFiMzdmMjM1NTFhYiIsImlhdCI6MTc3NTU3MDcwNSwiZXhwIjoyMDkwOTMwNzA1fQ.v6OQWDMW3EuA-cQotcnzCZboLtpMe3ezANFj36shPG4",
+    homeAssistantUrl: "URL Home Assistant",
+    accessToken: "TOKEN",
     updateInterval: 15 * 60 * 1000,
     selectedChild: 0,
     selectedDayOffset: 1,
@@ -16,18 +16,18 @@
         id: "jade",
         name: "JADE",
         entities: {
-          homework: "sensor.pronote_leperchey_gomas_jade_homework",
-          timetableToday: "sensor.pronote_leperchey_gomas_jade_today_s_timetable",
-          timetableTomorrow: "sensor.pronote_leperchey_gomas_jade_tomorrow_s_timetable"
+          homework: "sensor.pronote_.................s_jade_homework",
+          timetableToday: "sensor.pronote_................._jade_today_s_timetable",
+          timetableTomorrow: "sensor.pronote_.............._jade_tomorrow_s_timetable"
         }
       },
       {
         id: "alya",
         name: "ALYA",
         entities: {
-          homework: "sensor.pronote_leperchey_gomas_alya_homework",
-          timetableToday: "sensor.pronote_leperchey_gomas_alya_today_s_timetable",
-          timetableTomorrow: "sensor.pronote_leperchey_gomas_alya_tomorrow_s_timetable"
+          homework: "sensor.pronote_............._alya_homework",
+          timetableToday: "sensor.pronote_..............._alya_today_s_timetable",
+          timetableTomorrow: "sensor.pronote_................_alya_tomorrow_s_timetable"
         }
       }
     ]
